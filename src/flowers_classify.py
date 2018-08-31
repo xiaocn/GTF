11568604bac1d934bbe537be21920b327e429681
xiaocn <lixiao_img@163.com>
2018-08-31 19:46:21 +0800

    学习slim

--- src/flowers_classify.py
+++ src/flowers_classify.py
@@ -33,44 +33,75 @@ def create_image_tfrecord():
                 img = Image.open(os.path.join(data_basepath,class_name,image_name))
                 size = img.size
                 img_raw = img.tobytes()  # 将图片转化为二进制格式
-              #  print(class_dict[class_name])
+                print(class_dict[class_name])
                 example = tf.train.Example(features=tf.train.Features(feature={
                     'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[class_dict[class_name]])),
                     'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw])),
                     'img_width': tf.train.Feature(int64_list=tf.train.Int64List(value=[size[0]])),
-                    'img_height': tf.train.Feature(int64_list=tf.train.Int64List(value=[size[1]])),
-                    'img_name':tf.train.Feature(bytes_list=tf.train.BytesList(value=[np.array(image_name).tobytes()]))
+                    'img_height': tf.train.Feature(int64_list=tf.train.Int64List(value=[size[1]]))
                 }))
                 writer.write(example.SerializeToString())  # 序列化为字符串
         writer.close()
 
 
+def readTFrecord(tfrecord_path,type_dict):
+    features = model.read_tfrecord(os.path.join(tfrecord_path, 'test', type_dict['test']), {
+        'label': tf.FixedLenFeature([], tf.int64),
+        'img_raw': tf.FixedLenFeature([], tf.string),
+        'img_width': tf.FixedLenFeature([], tf.int64),
+        'img_height': tf.FixedLenFeature([], tf.int64)
+    })
+    # image = tf.image.decode_image(features['img_raw'],3)
+    label = tf.cast(features['label'], tf.int32)
+    # image_re = tf.reshape(image,shape=[100,200,3],name='reshape')
+    return label
 
 def reader():
     tfrecord_path = '/media/ai/data/workrooms/datas/tfrecord/flowers'
     type_dict = {'test': 'testdata.tfrecords-00000', 'train': 'traindata.tfrecords-00000', 'val': 'valdata.tfrecords-00000'}
-    features = model.read_tfrecord(os.path.join(tfrecord_path,'test',type_dict['test']), {
-        'label': tf.FixedLenFeature([], tf.int64),
-        'img_raw': tf.FixedLenFeature([], tf.string),
-        'img_width': tf.FixedLenFeature([], tf.int64),
-        'img_height': tf.FixedLenFeature([], tf.int64),
-        'img_name':tf.FixedLenFeature([],tf.string)
-    })
-    #image = tf.image.decode_image(features['img_raw'],3)
-    label = tf.cast(features['label'],tf.int32)
-    #image_re = tf.reshape(image,shape=[100,200,3],name='reshape')
-    labelbatch,batch_image_name = tf.train.batch([label,features['img_name']],16,num_threads=10,capacity=64)
+    label = readTFrecord(tfrecord_path,type_dict)
+    labelbatch = tf.train.batch([label],16,num_threads=10,capacity=64)
     with tf.Session() as sess:  # 开始一个会话
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess, coord=coord)
-        label_value,batch_image_value = sess.run([labelbatch,batch_image_name])
+        label_value = sess.run(labelbatch)
         for step in range(1000):
-            print('step %d, label is %s, image_name is %s' %(step,'{}'.format(label_value),'{}'.format(batch_image_value)))
+            print('step %d, label is %s ' %(step,'{}'.format(label_value)))
         coord.request_stop()
         coord.join(threads)
 
 
 if __name__ == "__main__":
-    reader()
+    """Queue
+    q = tf.FIFOQueue(3,"float")
+    init = q.enqueue_many(([0.1,0.2,0.3],))
+    x = q.dequeue()
+    y = x+1
+    q_inc = q.enqueue([y])
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(2):
+            sess.run(q_inc)
+        quelen = sess.run(q.size())
+        for i in range(quelen):
+            print(sess.run(q.dequeue()))
+    """
+    q = tf.FIFOQueue(10,'float')
+    counter = tf.Variable(0.0)
+    increment_op = tf.assign_add(counter,1.0)
+    enqueue_op = q.enqueue(counter)
+    qr = tf.train.QueueRunner(q,enqueue_ops=[increment_op,enqueue_op]*2)
+    '''报错
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        enqueue_threads = qr.create_threads(sess,start=True)
+        for i in range(10):
+            print(sess.run(q.dequeue()))
+    '''
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    enqueue_threads = qr.create_threads(sess,start=True)
+    for i in range(10):
+        print(sess.run(q.dequeue()))
---
+++ src/slims/test.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+dataset = slim.data_provider
